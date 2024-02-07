de64f5167b84de7fe366590738ba8227dea91633
Rishi <rishiekandala@gmail.com>
2024-02-07 14:12:19 +0530

    single layout children

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        title: Text('Flutter Developer'),
-      ),
-      body: const Center(
-        child: Image(
-            image: AssetImage('assets/employee-rishie.jpeg')
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.pink,
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Text('Flutter Developer'),
+        ),
+        body: Center(
+          child: Container(
+            height:200,
+            width:200,
+            color: Colors.blue,
+            child: Center(
+              child: Text('Container'),
+            ),
+          )
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
