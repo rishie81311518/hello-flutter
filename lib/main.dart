207c39b1aaf0b47a5652dca1dd755ac30c54c5e7
Rishi <rishiekandala@gmail.com>
2024-02-07 10:45:36 +0530

    adding assets

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,11 @@ void main() {
       backgroundColor: Colors.red,
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
-        title: Text('I am Flutter Developer'),
+        title: Text('Flutter Developer'),
       ),
       body: const Center(
         child: Image(
-            image: NetworkImage(
-                'https://media.istockphoto.com/id/1300512215/photo/headshot-portrait-of-smiling-ethnic-businessman-in-office.jpg?s=1024x1024&w=is&k=20&c=tq1C4HoZraH5szwBZqUf7sGVp7EPO-VB8PVbAPjagqY=')
+            image: AssetImage('assets/employee-rishie.jpeg')
         ),
       ),
     ),
