c5f5f3d3fa21f6d9d0f6f2bd91b73f4d9da8b7db
Rishi <rishiekandala@gmail.com>
2024-02-07 10:23:07 +0530

    scaffolding material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Center(child: Text('Hello World'),
-  ),
+  runApp( MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('I am Flutter Developer'),
+      ),
+      body: const Center(
+        child: Image(
+            image: NetworkImage(
+                'https://media.istockphoto.com/id/1300512215/photo/headshot-portrait-of-smiling-ethnic-businessman-in-office.jpg?s=1024x1024&w=is&k=20&c=tq1C4HoZraH5szwBZqUf7sGVp7EPO-VB8PVbAPjagqY=')
+        ),
+      ),
+    ),
   ));
 }
